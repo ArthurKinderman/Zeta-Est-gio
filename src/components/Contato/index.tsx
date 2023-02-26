456a6da42432012725d7c0ef17f350223db7ea10
Arthur Kinderman <88434239+ArthurKinderman@users.noreply.github.com>
2023-02-26 15:21:28 -0300

    finalized homepage

--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -5,7 +5,22 @@ const Contato = () =>{
     
     return(
         <Section>
-            
+            <form id="contato">
+                <div className="container">
+                    <legend>Contato</legend>
+
+                    <label htmlFor="name">Nome:</label>
+                    <input type="name"/>
+
+                    <label htmlFor="email">Email:</label>
+                    <input type="email"/>
+
+                    <label htmlFor="tell">Telefone:</label>
+                    <input type="tell"/>
+
+                    <button type="submit">Enviar</button>
+                </div>
+            </form>
         </Section>
     )
 }
--- src/components/Contato/styles.ts
+++ src/components/Contato/styles.ts
@@ -3,6 +3,44 @@ import styled from "styled-components";
 
 export const Section = styled.div`
 
-      
-   
+    form{
+        display: flex;
+        width: 480px;
+        justify-content:space-between;
+        margin-left: auto;
+        margin-right: auto;
+    }
+    .container{
+        background: white; 
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        border-radius: 2px;
+        margin-bottom: 50px;
+        padding-bottom: 20px;
+        padding-top: 10px;
+        border: 1px solid black;
+        border-radius: 4px;
+    }
+    legend , label{
+        text-align: center;
+        color: black;
+    }
+
+    input{
+        margin: 10px;
+        width: 320px;
+        height: 45px;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    button{
+        background: white;
+        width: 70px;
+        margin: auto;
+        border: 1px solid black;
+        border-radius: 4px;
+    }
 `
